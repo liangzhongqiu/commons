bb5fe276e56462dcbb92bb45c91e92736192884b
liangzhongqiu <liangzhongqiu789@sina.com>
2016-02-02 11:44:15 +0800

    修正二维码

--- src/main/java/com/lzq/commons/Quickmark.java
+++ src/main/java/com/lzq/commons/Quickmark.java
@@ -6,7 +6,6 @@ import java.util.Hashtable;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 /**
  * 
@@ -25,7 +24,7 @@ public abstract class Quickmark {
 	 * @return 缓冲图片
 	 * @throws WriterException
 	 */
-	public static BufferedImage fromQuickmark(String markContent,int width,int height,String charsetName) throws WriterException{
+	public static BufferedImage fromQuickmark(String markContent,int width,int height,String charsetName) throws Exception{
 		Hashtable<EncodeHintType,String> hins = new Hashtable<>();
 		hins.put(EncodeHintType.CHARACTER_SET,charsetName);
 		BitMatrix bitMatrix = new MultiFormatWriter().encode(markContent, BarcodeFormat.QR_CODE,width,height,hins);
